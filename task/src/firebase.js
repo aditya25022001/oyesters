ece48eed2e13a074b43e34128d0ed5168bf096a2
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-04-07 14:34:48 +0530

    hosted

---
+++ task/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCfeCKs5Vt1amrU_xt0nqOHcXBHpnLHpGQ",
+  authDomain: "todoapp-ca23f.firebaseapp.com",
+  databaseURL: "https://todoapp-ca23f-default-rtdb.firebaseio.com",
+  projectId: "todoapp-ca23f",
+  storageBucket: "todoapp-ca23f.appspot.com",
+  messagingSenderId: "429722728213",
+  appId: "1:429722728213:web:2d1fd03f5612f0e4929b1c",
+  measurementId: "G-7KJ05PVLGJ"
+};
